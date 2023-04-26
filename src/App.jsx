837c0ad2fed66c7252ede12b10feea7a606e553f
Janice Chen <janicechen31997@hotmail.com>
2023-04-25 19:08:13 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/owl-carousel/assets/css/custom.css';
 import '../node_modules/animate.css/animate.css';
-import "./css/style.css"; 
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 
